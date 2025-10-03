36e25d66429acdcf355c5154a7436d2cf4d0928e
Your Name <you@example.com>
2025-10-03 17:57:11 +0530

    adding js

---
+++ script.js
@@ -0,0 +1,26 @@
+// const header = document.querySelector('header');
+// const headerChanger = document.addEventListener ('scroll',()=>{
+// if (window.scrollY > 50){
+
+// header.style.background = 'red';
+// }
+// else
+// header.style.background = 'transparent';
+
+   
+// })
+
+    // document.addEventListener('DOMContentLoaded', () => {
+    //     const header = document.querySelector('header');
+
+    //     window.addEventListener('scroll', () => {
+    //         if (window.scrollY > 10) {
+    //             header.classList.add('scrolled');
+    //             console.log("Working ")
+    //         } else {
+    //             header.classList.remove('scrolled');
+    //         }
+    //     });
+    // });
+
+    
